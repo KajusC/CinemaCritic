0f8a32663a7dec63f6dba034279a569b7fa4e2fb
Jonas Šileikis <jonsil2@ktu.lt>
2024-04-26 14:23:01 +0300

    Made review form to be visually appealing by closely following the style of signup/login css style. added stars ratings but not final, merely a demo

--- CinemaCriticSolutionOnline/CinemaCritic.API/obj/Debug/net8.0/CinemaCritic.API.AssemblyInfo.cs
+++ CinemaCriticSolutionOnline/CinemaCritic.API/obj/Debug/net8.0/CinemaCritic.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CinemaCritic.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ce0f47c3e4ed2266c2f05028678ddad1e126bf4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5934d92423a8406916278a81810de5b11d607c5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CinemaCritic.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CinemaCritic.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
