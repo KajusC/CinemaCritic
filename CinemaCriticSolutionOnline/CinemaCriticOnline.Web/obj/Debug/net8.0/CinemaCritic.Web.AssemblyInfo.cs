7c50eebada15b52b00b1ed4f3b1a33ba67a61529
Kajus CC <42713684+KajusC@users.noreply.github.com>
2024-05-01 12:16:15 +0300

    random files in site

--- CinemaCriticSolutionOnline/CinemaCriticOnline.Web/obj/Debug/net8.0/CinemaCritic.Web.AssemblyInfo.cs
+++ CinemaCriticSolutionOnline/CinemaCriticOnline.Web/obj/Debug/net8.0/CinemaCritic.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CinemaCritic.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5934d92423a8406916278a81810de5b11d607c5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f8a32663a7dec63f6dba034279a569b7fa4e2fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("CinemaCritic.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CinemaCritic.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
